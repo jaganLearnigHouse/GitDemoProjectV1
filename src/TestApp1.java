98705cf9ef4987c95071e6536c0cf9c79bc67691
kjagannnathreddy <kayajaga@gmail.com>
2019-07-28 22:59:04 +0530

    commiting first time

---
+++ src/TestApp1.java
@@ -0,0 +1,13 @@
+
+public class TestApp1 {
+
+	public static void main(String args[]){
+		String str1 = "happy";
+		
+		if(str1 == "happy"){
+			System.out.println("Do office work....");
+		}else
+			System.out.println("Go to goa...");
+		
+	}
+}
