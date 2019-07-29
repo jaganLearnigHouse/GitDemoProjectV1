a00f248823efd0b52459f47c847d5bf39a5e7e21
kjagannnathreddy <kayajaga@gmail.com>
2019-07-29 16:27:00 +0530

    commiting

---
+++ src/TestApp2.java
@@ -0,0 +1,14 @@
+
+public class TestApp2 {
+
+	public static void main(String args[]){
+		String str1 = "happy";
+		//cheking the happy condition having logics addinfdnfldfldfl
+		if(str1 == "happy"){
+			System.out.println("Do office work..and personal work..Do work....");
+			System.out.println("Do office work..and personal work..");
+		}else
+			System.out.println("Go to OOty...");
+		
+	}
+}
