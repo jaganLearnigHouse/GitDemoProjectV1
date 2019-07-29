ec0e9af7e8f75730bc9d3a5e6df6507b841b3c80
kjagannnathreddy <kayajaga@gmail.com>
2019-07-29 16:14:42 +0530

    commit21

--- src/TestApp1.java
+++ src/TestApp1.java
@@ -3,9 +3,10 @@ public class TestApp1 {
 
 	public static void main(String args[]){
 		String str1 = "happy";
-		
+		//cheking the happy condition having logics addinfdnfldfldfl
 		if(str1 == "happy"){
 			System.out.println("Do office work..and personal work..Do work....");
+			System.out.println("Do office work..and personal work..");
 		}else
 			System.out.println("Go to OOty...");
 		
